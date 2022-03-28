8657c83a3606bc94280e2feae1bb817242cd9ba3
emanamoura <emanuel.moura@ccc.ufcg.edu.br>
2022-03-28 15:19:53 -0300

    Handle transaction modal, creating inital styles and configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import { Dashboard } from './components/Dashboard';
+import Modal from 'react-modal';
 import { Header } from './components/Header'
+import { useState } from 'react';
+import { NewTransactionModal } from './components/NewTransactionModal';
+
+Modal.setAppElement('#root')
 
 function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+      setIsNewTransactionModalOpen(true) 
+  }
+
+  function handleCloseNewTransactionModal() {
+      setIsNewTransactionModalOpen(false)
+  }
+
+
   return (
     <>
-      <Header/>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard/>
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen} 
+        onRequestClose={handleCloseNewTransactionModal}/>
     </>
   );
 }
